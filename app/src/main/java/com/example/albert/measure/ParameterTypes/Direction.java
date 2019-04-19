839166c1d4ef39ce349710ad58adc9f170b7e15f
Albert Canales Ros <albertcanalesros@gmail.com>
2019-04-19 12:45:42 +0200

    Step Input Created

---
+++ app/src/main/java/com/example/albert/measure/ParameterTypes/Direction.java
@@ -0,0 +1,34 @@
+package com.example.albert.measure.ParameterTypes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.albert.measure.fragments.TwoOptionParameterFragment;
+
+public class Direction extends TwoOptionParameterFragment {
+
+    public Direction() {
+        super("PARALLEL", "PERPENDICULAR");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == first)
+            passData("PARALLEL");
+        else
+            passData("PERPENDICULAR");
+    }
+
+    public void passData(String data) {
+        dataPasser.onDataPass(data);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/albert/measure/ParameterTypes/Plane.java
@@ -0,0 +1,34 @@
+package com.example.albert.measure.ParameterTypes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.albert.measure.fragments.TwoOptionParameterFragment;
+
+public class Plane extends TwoOptionParameterFragment {
+
+    public Plane() {
+        super("HORIZONTAL", "VERTICAL");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == first)
+            passData("HORIZONTAL");
+        else
+            passData("VERTICAL");
+    }
+
+    public void passData(String data) {
+        dataPasser.onDataPass(data);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
